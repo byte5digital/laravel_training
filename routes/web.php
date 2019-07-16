bb45392b2faacf0292c8b91f5ef891ccdd2424e3
Marcel Wege <mwege@byte5.de>
2019-07-16 11:15:38 +0200

    Add Test Middleware

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/projects', 'ProjectController@index')->name('project.index');
+// Route::middleware(['test'])->get('/projects', 'ProjectController@index')->name('project.index');
